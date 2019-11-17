006da1a6aa513d1d021d0d2aa7cc8182b13e331a
Vladholovchak <vlad.golovchak.1996@gmail.com>
2019-11-17 18:17:58 +0200

    Sceleton of project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Book.create(name: "In Search of Lost Time", author:"Marcel Proust", pages: 123, price: 70)
+  Book.create(name: "Ulysses", author: "James Joyce", pages: 123, price: 70)
+  Book.create(name: "Don Quixote", author:"Miguel de Cervantes", pages: 123, price: 70)
+  Book.create(name: " The Great Gatsby", author:"F. Scott Fitzgerald", pages: 123, price: 70)
+  Book.create(name: "Moby Dick", author:"Herman Melville", pages: 123, price: 70)
+  Book.create(name: "War and Peace", author:"Leo Tolstoy", pages: 123, price: 70)
+  Book.create(name: "Hamlet", author:"William Shakespeare", pages: 123, price: 70)
+  Book.create(name: "Lolita", author:"Vladimir Nabokov", pages: 123, price: 70)
+  Book.create(name: "Madame Bovary", author:"Gustave Flaubert", pages: 123, price: 70)
+  Book.create(name: "The Catcher in the Rye ", author:"J. D. Salinger", pages: 123, price: 70)
+  Book.create(name: "Crime and Punishment", author:"Fyodor Dostoyevsky", pages: 123, price: 70)
